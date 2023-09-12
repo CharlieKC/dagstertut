4a443f7b6616d3879aaa34a68c5a49df9917ef6a
Charlie Kruczko <charlie.kruczko@gmail.com>
2023-09-12 11:52:21 +0100

    Work on filewatching API

--- dagstertut/__init__.py
+++ dagstertut/__init__.py
@@ -19,6 +19,9 @@ all_assets = load_assets_from_modules([assets, filewatching])
 # Addition: define a job that will materialize the assets
 hackernews_job = define_asset_job("hackernews_job", selection=AssetSelection.all())
 
+# Add job from the filewatching
+
+
 # Addition: a ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
 hackernews_schedule = ScheduleDefinition(
     job=hackernews_job,
@@ -38,6 +41,8 @@ datagen = DataGeneratorResource(num_days=365)
 defs = Definitions(
     assets=all_assets,
     schedules=[hackernews_schedule],
+    jobs=[filewatching.asset_job],
+    sensors=[filewatching.my_directory_sensor],
     resources={
         "io_manager": io_manager,
         "database_io_manager": database_io_manager,
--- dagstertut/filewatching.py
+++ dagstertut/filewatching.py
@@ -1,9 +1,14 @@
 import os
 from pathlib import Path
 
-from dagster import Config, RunConfig, RunRequest, job, op, sensor, define_asset_job, asset
+from dagster import AssetSelection, Config, RunConfig, RunRequest, job, op, sensor, define_asset_job, asset, DefaultSensorStatus
 
-MY_DIRECTORY = Path(__file__).parent.parent / "data"
+FILEWATCHING_DIR = Path(__file__).parent.parent / "watchdir"
+INPUT_DIR = FILEWATCHING_DIR / "input"
+OUTPUT_DIR = FILEWATCHING_DIR / "output"
+
+assert INPUT_DIR.is_dir(), f"input dir not found {INPUT_DIR}"
+assert OUTPUT_DIR.is_dir(), f"output dir not found {OUTPUT_DIR}"
 
 class FileConfig(Config):
     filename: str
@@ -18,22 +23,27 @@ def process_file(context, config: FileConfig):
 def log_file_job():
     process_file()
 
-asset_job = define_asset_job("asset_job", "*")
-
-
-@sensor(job=asset_job)
-def my_directory_sensor():
-    for filename in os.listdir(MY_DIRECTORY):
-        filepath = os.path.join(MY_DIRECTORY, filename)
-        if os.path.isfile(filepath):
-            yield RunRequest(
-                run_key=filename,
-                run_config=RunConfig(
-                    ops={"process_file": FileConfig(filename=filename)}
-                ),
-            )
-
-
 @asset
 def ret1():
     return 1
+
+
+@asset
+def ret2(ret1):
+    return 2
+
+asset_job = define_asset_job(name="asset_job", selection=AssetSelection.keys("ret1", "ret2"))
+
+
+@sensor(job=asset_job, default_status=DefaultSensorStatus.RUNNING)
+def my_directory_sensor():
+    for filename in os.listdir(INPUT_DIR):
+        filepath = os.path.join(INPUT_DIR, filename)
+        if os.path.isfile(filepath):
+            yield RunRequest(
+                run_key=filename,
+                # run_config=RunConfig(
+                    # ops={"process_file": FileConfig(filename=filename)}
+                # ),
+            )
+
